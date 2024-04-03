41f7c6904a05e3d0e9b642928818f62cc589bcca
ShivanshuPrajapati212 <152085574+ShivanshuPrajapati212@users.noreply.github.com>
2024-04-03 21:24:45 +0530

    f

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
 from win10toast import ToastNotifier
-import pandas as pd
+from datetime import datetime
 
 def show_notification(title, message):
     toaster = ToastNotifier()
     toaster.show_toast(title, message, duration=2)
 
 
-if __name__ == "__main__":
-    task = input("Enter the Name of Task: ")
-    desc = input("Give some Description about the task: ")
-    time  = input("Enter the Time to Schedule (22:00): ")
 
-    show_notification(task, desc)
+
+if __name__ == "__main__":
+
+    with open('D:\\Notifier in Python\\task.txt') as f:
+        task_file = f.read()
+    task_file = task_file.split('-')
+    task_time = task_file[2]
+    task_time = float(task_time)
+    now_time = datetime.now()
+    now_time = float(now_time.timestamp())
+    if task_time <= now_time:
+        show_notification(task_file[0], task_file[1])
+    else:
+        pass
